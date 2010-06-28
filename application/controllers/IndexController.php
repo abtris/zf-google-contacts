60bca09c90fd765782f9e5cb3aca750ac342178e
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2010-06-28 11:15:12 +0200

    Update contacts

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,9 +10,57 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+
     }
 
+    public function contactsAction()
+    {
+        // action body
 
+        $self = 'http://' . $_SERVER['SERVER_NAME'] . $this->view->url(array('controller'=>'index','action'=>'contacts'));
+        //Zend_Debug::dump($this->getRequest());
+        $token = $this->getRequest()->getParam('token');
+        if (isset($token)) {
+            Zend_Registry::set('contact_token', Zend_Gdata_AuthSub::getAuthSubSessionToken($token));
+            header('Location: ' . $self);
+            exit;
+        }
+
+        if (!isset(Zend_Registry::get('contact_token'))) {
+            $scope = 'http://www.google.com/m8/feeds';
+            $uri = Zend_Gdata_AuthSub::getAuthSubTokenUri($self, $scope, 0, 1);
+            header('Location: ' . $uri);
+            exit;
+        }
+
+        $client = Zend_Gdata_AuthSub::getHttpClient(Zend_Registry::get('contact_token'));
+        $gdata = new Zend_Gdata($client);
+        $query = new Zend_Gdata_Query('http://www.google.com/m8/feeds/contacts/default/full');
+        $feed = $gdata->getFeed($query);
+        $entries = $gdata->retrieveAllEntriesForFeed($feed);
+
+        $contacts = array();
+
+
+        foreach ($entries as $entry) {
+            $name = $entry->title->text;
+            $email = null;                                                                                                                                           
+            $extensions = $entry->getExtensionElements();
+            foreach ($extensions as $extension) {
+                if ($extension->rootElement == 'email') {
+                    $attributes = $extension->getExtensionAttributes();
+                    $email = $attributes['address']['value'];
+                }
+            }
+            if ($email) {
+                $contacts[] = array('name' => $name, 'email' => $email);
+            }
+        }
+
+        foreach ($contacts['email'] as $email) {
+
+            echo $email.",";
+        }
+    }
 }
 
